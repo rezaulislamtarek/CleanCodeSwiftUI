7a1a7f41fd892e4ef73841b24b06978f5af0e260
rezaulislamtarek <mrezaulislamtarek.t@gmail.com>
2024-05-28 16:20:26 +0600

    Initial Commit

---
+++ CleanCodeSwiftUI/CleanCodeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanCodeSwiftUIApp.swift
+//  CleanCodeSwiftUI
+//
+//  Created by Rezaul Islam on 28/5/24.
+//
+
+import SwiftUI
+
+@main
+struct CleanCodeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
